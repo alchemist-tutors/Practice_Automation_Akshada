1ddcf30fadec0c67f509de490e5276a005c35752
alche <alche@GRATITUDE>
2024-04-12 22:38:58 +0530

    commited

---
+++ test_java/src/test_java/Test_msg.java
@@ -0,0 +1,11 @@
+package test_java;
+
+public class Test_msg {
+
+	public static void main(String[] args) {
+
+   System.out.println("java");
+
+	}
+
+}
---
+++ test_java/src/test_java/test.java
@@ -0,0 +1,11 @@
+package test_java;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("java");
+		
+	}
+
+}
